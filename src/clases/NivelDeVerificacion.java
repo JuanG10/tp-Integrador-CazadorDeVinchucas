7ee6e256a63757b3bd046d6a1a9c7a9ed4011a9b
w-etc <luwolf7@gmail.com>
2018-11-09 10:11:46 -0300

    eliminados metodos sin uso

--- src/clases/NivelDeVerificacion.java
+++ src/clases/NivelDeVerificacion.java
@@ -5,14 +5,6 @@ import java.util.List;
 
 public abstract class NivelDeVerificacion {
 	
-	public abstract boolean esIgualA(NivelDeVerificacion nivel);
-	
-	public abstract boolean esAlto();
-	
-	public abstract boolean esMedio();
-	
-	public abstract boolean esBajo();
-
 	public static Verificacion verificacionPara(Usuario usuario, String tipoDeVinchuca) {
 		return verificacionesPosibles().stream().filter(verificacion -> verificacion.puedeEncargarseDe(usuario, tipoDeVinchuca)).findFirst().get().encargarseDe(usuario,  tipoDeVinchuca);
 	}
--- src/clases/NivelDeVerificacionAlto.java
+++ src/clases/NivelDeVerificacionAlto.java
@@ -1,30 +1,6 @@
 package clases;
 
 public class NivelDeVerificacionAlto extends NivelDeVerificacion {
-	
-	public boolean esIgualA(NivelDeVerificacion nivel) {
-		return nivel.esAlto();
-	}
-	
-	public boolean esAlto() {
-		return true;
-	}
-	
-	public  boolean esMedio() {
-		return false;
-	}
-	
-	public boolean esBajo( ) {
-		return false;
-	}
-	
-	public boolean esMayorA(NivelDeVerificacion nivel) {
-		return nivel.esMedio() || nivel.esBajo();
-	}
-	
-	public boolean esMenorA(NivelDeVerificacion nivel) {
-		return false;
-	}
 
 	@Override
 	public boolean puedeEncargarseDe(Usuario usuario, String tipoDeVinchuca) {
