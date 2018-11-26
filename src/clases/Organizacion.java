b7a54e265d665374ba3c19b4282477896190bdcc
Facu <pache.facundo@gmail.com>
2018-11-26 20:16:53 -0300

    metodos para agregar y remover zonas de covertura a la organizacion

--- src/clases/Organizacion.java
+++ src/clases/Organizacion.java
@@ -1,11 +1,14 @@
 package clases;
 
 
+import java.util.ArrayList;
+
 public abstract class Organizacion{
 
 	private String tipoDeOrganizacion;
 	private Ubicacion ubicacion;
 	private Integer cantPersonas;
+	private ArrayList <ZonaDeCobertura> zonasDeCobertura = new ArrayList<ZonaDeCobertura>();
 	
 	public Organizacion(String tipo, Ubicacion ubicacion, Integer cantPersonas) {
 		this.tipoDeOrganizacion = tipo;
@@ -13,6 +16,12 @@ public abstract class Organizacion{
 		this.cantPersonas = cantPersonas;
 	}
 	
+	public void registrarZonaDeCobertura(ZonaDeCobertura zona) {
+		this.zonasDeCobertura.add(zona);
+	}
+	public void removerZonaDeCobertura(ZonaDeCobertura zona) {
+		this.zonasDeCobertura.remove(zona);
+	}
 	
 	//GETERS
 	public String tipoDeOrganizacion() {
