2de9812f114606a9965335043977a88a66fcf8e8
Facu <pache.facundo@gmail.com>
2018-11-26 20:22:06 -0300

    Se creo la interfaz FuncionalidadExterna

---
+++ src/interfaces/FuncionalidadExtenerna.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import clases.Muestra;
+import clases.Organizacion;
+import clases.ZonaDeCobertura;
+
+public interface FuncionalidadExtenerna {
+
+	public void nuevoEvento(Organizacion org, ZonaDeCobertura zona, Muestra muestra);
+}
