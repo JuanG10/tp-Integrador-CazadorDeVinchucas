29899679b7f079eacf0f5e0b58ae18ccd8e273dc
w-etc <luwolf7@gmail.com>
2018-11-30 18:41:25 -0300

    arreglando cagadas

--- src/clases/ConocimientoEspecialista.java
+++ src/clases/ConocimientoEspecialista.java
@@ -11,6 +11,13 @@ public class ConocimientoEspecialista implements NivelDeConocimiento {
 			List<Muestra> muestrasEnviadas) {
 		return this;
 	}
+
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		if (o == this) return true;
+		if (o instanceof ConocimientoEspecialista)return true;
+		return false;
+	}
 	
 	public boolean esDefinitorio() {
 		return true;
